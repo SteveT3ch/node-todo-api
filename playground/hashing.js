1a2eb96bee674d30b0a360cb1a9309746421a4d1
Steve Peters <steve.t3ch@gmail.com>
2018-09-09 17:27:36 -0400

    [*] Feature added to hash password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,32 @@
 const { log } = console;
 //const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const data = {
-  id: 10
-}
+const password = 'password123';
 
-const token = jwt.sign(data, '123acb');
-log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//       log(hash);
+//   })
+// });
 
-const decoded = jwt.verify(token, '123acb');
-log('decoded', decoded);
+const hash = '$2a$10$GgK7VPDbfL3FskeO2bEKhOIisBvW1oUFAgM5g7fqjMOho6/9Mva86'
 
+bcrypt.compare(password, hash, (err, res) => {
+  log(res);
+});
+
+// const data = {
+//   id: 10
+// }
+//
+// const token = jwt.sign(data, '123acb');
+// log(token);
+//
+// const decoded = jwt.verify(token, '123acb');
+// log('decoded', decoded);
+//
 
 
 // const message = 'i am user number 3';
